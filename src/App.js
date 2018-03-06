3e09ff721ed0859b10dc0b84dfd40e34e43b622b
Nayaya <nayayaibrahim@gmail.com>
2018-03-06 04:02:41 +0100

    created my first reusable component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {bootstrap} from './node_modules/bootstrap/dist/js/bootstrap.min.js';
+import Navigation from './components/Navigation'
 
 class App extends Component {
   render() {
-    return 
-      <div className="container-fluid" dark>
-            <div className="App">
-                <div className="nav">
-                    <div className="navbar">
-
-                    </div>
-                </div>
-            </div> 
+    return (
+      <div className="App">
+      <Navigation />
       </div>
+  );
   }
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div class="pos-f-t">
+            <div class="collapse" id="navbarToggleExternalContent">
+                <div class="bg-dark p-4">
+                    <h4 class="text-white">Collapsed content</h4>
+                    <span class="text-muted">Toggleable via the navbar brand.</span>
+                </div>
+            </div>
+        <nav class="navbar navbar-dark bg-dark" dark>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+        </div>
+      </div>
+  );
+  }
+}
+
+export default Navigation;
+
